eb0bd39b870af5011c59639872d840b8b64fee11
shrutivanw <shruti.dalvi@gmail.com>
2018-11-29 13:17:12 -0500

    Update matrix_convert_to_zero.rb
    Changing method name to matrix_convert_to_zero instead of matrix_convert_to_0

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -2,6 +2,6 @@
 # Assumption/ Given: All numbers in the matrix are 0s or 1s
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
-def matrix_convert_to_0(matrix)
+def matrix_convert_to_zero(matrix)
   raise NotImplementedError
 end
