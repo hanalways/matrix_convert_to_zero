0a7845a4df38269b340324f5312714b6a7b3c8ea
Hana <hanalways@gmail.com>
2019-05-01 21:41:50 -0700

    create helper methods

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -7,28 +7,54 @@
 # Space complexity: ?
 require 'pry'
 def matrix_convert_to_zero(matrix)
-  return_matrix = []
-  matrix_col = Hash.new
-  is_zero = false
+  return_matrix = create_matrix(matrix)
 
-  matrix.each do |row|
-    row.each_with_index do |num, i|
+  matrix.each_with_index do |row, i|
+    row.each_with_index do |num, j|
       if num == 0
-        is_zero = true
-        matrix_col[i] = 1
-      else
-        is_zero = false 
+        convert_row_to_zero(i, return_matrix)
+        convert_col_to_zero(j, return_matrix)
       end
     end
-    if is_zero == true
-      return_matrix << Array.new(3, 0)
-    else
-      return_matrix << Array.new(3, 1)
-    end
+  end  
+  
+  return return_matrix
+end
+
+def convert_row_to_zero(row, matrix)
+  i = 0
+  
+  while i < matrix[row].length
+    matrix[row][i] = 0
+    i += 1
   end
-  binding.pry
+
+  return matrix
+end
+
+def convert_col_to_zero(column, matrix)
+  i = 0
+
+  while i < matrix.length
+    matrix[i][column] = 0
+    i += 1
+  end
+
+  return matrix
+end
+
+def create_matrix(matrix)
+  return_matrix = Array.new
+  matrix.count.times do 
+    return_matrix << Array.new(matrix[0].count, 1)
+  end
+
   return return_matrix
 end
 
 matrix = [[1,1,1], [1,0,1], [1,1,1], [1,1,0]]
-puts matrix_convert_to_zero(matrix)
+
+return_matrix = create_matrix(matrix)
+# puts "#{convert_row_to_zero(1, create_matrix(return_matrix))}"
+# puts "#{convert_col_to_zero(1, create_matrix(return_matrix))}"
+puts "#{matrix_convert_to_zero(matrix)}"
